9cfb1e74fb5c54edf4b9479947ecab81822945f2
masterujjval <guptarickey3@gmail.com>
2025-11-13 10:28:02 +0000

    added egreesS

--- db_service.py
+++ db_service.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 
 class ChatHistoryDB:
-    def __init__(self, db_path='chat_history.db'):
+    def __init__(self, db_path='/app/uploads/chat_history.db'):
         self.db_path = db_path
         self.init_db()
     
